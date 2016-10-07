de419b1ad8646658ebbe0106a994d2c735ff4be7
ozPop <ozziepopovas@gmail.com>
2016-10-07 17:42:50 -0400

    update association in user

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,5 @@ class User < ApplicationRecord
   # as artist
   has_many :artworks, foreign_key: :artist_id
   has_many :sales, class_name: 'Transaction', foreign_key: :artwork_id
-  has_many :buyers, through: :sales, source: :transaction
+  has_many :buyers, through: :sales, foreign_key: :buyer_id
 end
