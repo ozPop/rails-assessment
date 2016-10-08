003b00bdd34c6a590f5a84e05b040ba8fcacdcd5
ozPop <ozziepopovas@gmail.com>
2016-10-08 16:23:21 -0400

    update transaction associations in models, edit migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   # as artist
   has_many :artworks, foreign_key: :artist_id
-  has_many :sales, class_name: 'Transaction', foreign_key: :order_id
+  has_many :sales, class_name: 'Transaction', foreign_key: :seller_id
   has_many :sold_orders, through: :sales, source: :order
-  has_many :buyers, through: :sold_orders, foreign_key: :buyer_id
+  has_many :buyers, through: :sales, foreign_key: :buyer_id
 end
