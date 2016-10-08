f8d2d7490fc917a02db291bb190cd1937e33052c
ozPop <ozziepopovas@gmail.com>
2016-10-08 13:13:12 -0400

    rename migrations cart -> order

--- db/migrate/20161008170841_create_orders.rb
+++ db/migrate/20161008170841_create_orders.rb
@@ -1,6 +1,6 @@
-class CreateCarts < ActiveRecord::Migration[5.0]
+class CreateOrders < ActiveRecord::Migration[5.0]
   def change
-    create_table :carts do |t|
+    create_table :orders do |t|
       t.integer :user_id
       t.string :status,   default: 'not submitted'
 
