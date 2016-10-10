500bab6d551c8ca681c16be271bbcbe1e372b88b
fidelscodes <fidelscodes@gmail.com>
2016-10-10 12:56:44 -0400

    implement omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-    :recoverable, :rememberable, :trackable, :validatable
+    :recoverable, :rememberable, :trackable, :validatable, :omniauthable
 
   # as buyer
   has_many :orders, foreign_key: :buyer_id
@@ -13,4 +13,13 @@ class User < ApplicationRecord
   has_many :artworks, foreign_key: :artist_id
   has_many :sales, through: :artworks, source: :transactions
   has_many :buyers, through: :sales
+
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+    end
+  end
+
 end
