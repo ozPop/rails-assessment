f93320425ed42fff185e973236753230eda6b07c
ozPop <ozziepopovas@gmail.com>
2016-10-11 10:59:06 -0400

    update order model with helper methods

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,39 @@
 class Order < ApplicationRecord
-  has_many :order_items
+  has_many :order_items, dependent: :destroy
   has_many :items, through: :order_items, source: :artwork
   has_many :transactions
   belongs_to :buyer, class_name: 'User'
+
+  def add_item(item_id)
+    order_item = order_items.find_by(artwork_id: item_id)
+    if order_item
+      order_item.quantity += 1
+    else
+      order_item = order_items.build(artwork_id: item_id)
+    end
+    order_item
+  end
+
+  def total
+    total = 0
+    order_items.each do |order_item|
+      total += order_item.artwork.price * order_item.quantity
+    end
+    total
+  end
+
+  def checkout
+    self.status = 'submitted'
+    update_inventory
+  end
+
+  def update_inventory
+    if self.status = 'submitted'
+      self.order_items.each do |order_item|
+        order_item.artwork.inventory -= order_item.quantity
+        order_item.artwork.save
+      end
+    end
+  end
+
 end
