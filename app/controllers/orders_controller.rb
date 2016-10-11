cbd87444bf60040c4ad9f02f8c56af52e49222cc
fidelscodes <fidelscodes@gmail.com>
2016-10-11 12:09:19 -0400

    add #set_order, checkout action to orders controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,18 @@
 class OrdersController < ApplicationController
+  before_action :set_order, only: [:show, :checkout]
+
   def show
     # render order show page
+  end
+
+  def checkout
+    @order.checkout
+    redirect_to root_path
+  end
+
+  private
+
+  def set_order
+    @order = Order.find_by(id: params[:id])
   end
 end
