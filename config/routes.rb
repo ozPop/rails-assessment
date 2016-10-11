a5df2545f42fb04cb73da9d1de6ca3d2bc749349
fidelscodes <fidelscodes@gmail.com>
2016-10-11 11:00:32 -0400

    add orders controller

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,7 @@ Rails.application.routes.draw do
 
   resources :order_items, only: [:create]
 
+  resources :orders, only: [:show]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
