57769eafc8bfa497e859e29d19fb1ec6afa345e3
ozPop <ozziepopovas@gmail.com>
2016-10-11 13:21:40 -0400

    add checkout_form, update orders show view & controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,14 @@
 class OrdersController < ApplicationController
+  before_action :set_order, only: [:show]
+
   def show
     # render order show page
   end
+
+  private
+
+  def set_order
+    @order = Order.find_by(id: params[:id])
+  end
+
 end
