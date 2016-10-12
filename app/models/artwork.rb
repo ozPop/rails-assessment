76b919eeebf157259e525fb401de340d424bc775
ozPop <ozziepopovas@gmail.com>
2016-10-12 11:29:39 -0400

    add .most_popular scope method

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -5,4 +5,12 @@ class Artwork < ApplicationRecord
   has_many :transactions
   has_attached_file :image
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
+  scope :most_popular, -> {
+    select('artworks.*, count(transactions.artwork_id) AS transactions_count')
+      .joins(:transactions)
+      .group('artwork_id')
+      .order('transactions_count desc')
+      .limit(5)
+  }
 end
