f8adc1bdf4b1275f1c35bbfc016ec52ceaa286d8
fidelscodes <fidelscodes@gmail.com>
2016-11-09 12:35:34 -0500

    update user serializer to exclude sales and purchases update artwork serializer to include image file name

--- app/serializers/artwork_serializer.rb
+++ app/serializers/artwork_serializer.rb
@@ -1,7 +1,5 @@
 class ArtworkSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :price, :category_id, :artist_id, :available, :inventory
-  # the line below lets us access the actual artist and cateory objects
-  # attributes :id, :title, :description, :price, :category, :artist, :available, :inventory, :image_file_name
+  attributes :id, :title, :description, :price, :category_id, :artist_id, :available, :inventory, :image_file_name
   belongs_to :artist
   belongs_to :category
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,4 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :email, :seller
   has_many :artworks
-  has_many :sales
-  has_many :purchases
 end
