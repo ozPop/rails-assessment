029e56f5f8f051946aac8cfe18e6a2c80189aa66
ozPop <ozziepopovas@gmail.com>
2016-11-10 14:07:03 -0500

    cleaned up artwork.js

--- app/assets/javascripts/artwork.js
+++ app/assets/javascripts/artwork.js
@@ -1,11 +1,3 @@
-// $(document).on('turbolinks:load', function() {
-//   // make sure we are on the right page
-//   // in this case, the artworks/show
-//   if($('.artworks.show').length !== 0) {
-//     getArtwork();
-//   }
-// });
-
 function getArtwork(url) {
   $.ajax({
     url: url,
@@ -31,18 +23,9 @@ class Artwork {
   }
 
   // formatter instance method
-  // this might me useful on the artwork/show
   renderHTML() {
     let html = `<h1>${this.title}</h1>`;
     html += `<img class="img-responsive thumbnail" width="800" alt="${this.title}" src="/system/images/${this.image_file_name}" />`;
     return html;
-      // <h1>Artwork Show Page</h1>
-      // <hr />
-      // <%= content_tag :div, class: 'row' do %>
-      // <%= render partial: 'artwork', locals: { artwork: @artwork } %>
-      // <% end %>
-
-      console.log(`Title: ${this.title}`);
-    // code here...
   }
 }
