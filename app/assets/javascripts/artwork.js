5c2a7dcfe9b1202f20ec703c82c89c1e51b94a97
fidelscodes <fidelscodes@gmail.com>
2016-11-09 19:57:38 -0500

    update Artwork constructor to include artist update Artwork#renderHTML() add getArtwork() for requesting individual artwork

--- app/assets/javascripts/artwork.js
+++ app/assets/javascripts/artwork.js
@@ -1,16 +1,48 @@
+// $(document).on('turbolinks:load', function() {
+//   // make sure we are on the right page
+//   // in this case, the artworks/show
+//   if($('.artworks.show').length !== 0) {
+//     getArtwork();
+//   }
+// });
+
+function getArtwork(url) {
+  $.ajax({
+    url: url,
+    type: 'get',
+    dataType: 'json',
+    success: function(response){
+      let $target = $('.main-container');
+      let html = new Artwork(response.artwork).renderHTML();
+      $target.html(html);
+      console.log('Response: ', response.artwork);
+    }
+  });
+}
+
 class Artwork {
-  constructor({id, title, description, price, image_file_name}) {
+  constructor({id, artist, title, description, price, image_file_name}) {
     this.id = id;
     this.title = title;
     this.description = description;
     this.price = price;
     this.image_file_name = image_file_name;
+    this.artist = artist;
   }
 
   // formatter instance method
   // this might me useful on the artwork/show
   renderHTML() {
-    console.log('this would be html or handlebars...');
+    let html = `<h1>${this.title}</h1>`;
+    html += `<img class="img-responsive thumbnail" width="800" alt="${this.title}" src="/system/images/${this.image_file_name}" />`;
+    return html;
+      // <h1>Artwork Show Page</h1>
+      // <hr />
+      // <%= content_tag :div, class: 'row' do %>
+      // <%= render partial: 'artwork', locals: { artwork: @artwork } %>
+      // <% end %>
+
+      console.log(`Title: ${this.title}`);
     // code here...
   }
 }
